985ed24dd4d58254cf5905b6b478ab9dd45209ae
Luke <luca.mannella@outlook.it>
2016-01-12 03:39:14 +0100

    Annotation added.

--- src/it/polito/dp2/WF/sol4/server/WorkflowController.java
+++ src/it/polito/dp2/WF/sol4/server/WorkflowController.java
@@ -1,6 +1,14 @@
 package it.polito.dp2.WF.sol4.server;
 
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
+
 import it.polito.dp2.WF.sol4.gen.Actor;
+import it.polito.dp2.WF.sol4.gen.ObjectFactory;
 import it.polito.dp2.WF.sol4.gen.UnknownActionName_Exception;
 import it.polito.dp2.WF.sol4.gen.UnknownActorName_Exception;
 import it.polito.dp2.WF.sol4.gen.UnknownCode_Exception;
@@ -8,8 +16,18 @@ import it.polito.dp2.WF.sol4.gen.UnknownNames_Exception;
 import it.polito.dp2.WF.sol4.gen.UnknownNextActionName_Exception;
 import it.polito.dp2.WF.sol4.gen.WorkflowControllerInterface;
 
+@WebService(name = "WorkflowControllerInterface", 
+			targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/",
+			serviceName = "WorkflowService",
+			portName = "WorkflowControllerPort",
+			endpointInterface = "it.polito.dp2.WF.sol4.gen.WorkflowControllerInterface")
+@XmlSeeAlso({ObjectFactory.class})
 public class WorkflowController implements WorkflowControllerInterface {
 
+	@WebMethod
+    @WebResult(name = "result", targetNamespace = "")
+    @RequestWrapper(localName = "createNewProcess", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.CreateNewProcess")
+    @ResponseWrapper(localName = "createNewProcessResponse", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.CreateNewProcessResponse")
 	@Override
 	public boolean createNewProcess(String wfName) throws UnknownNames_Exception {
 		
@@ -18,6 +36,10 @@ public class WorkflowController implements WorkflowControllerInterface {
 		return false;
 	}
 
+	@WebMethod
+    @WebResult(name = "result", targetNamespace = "")
+    @RequestWrapper(localName = "takeOverAction", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.TakeOverAction")
+    @ResponseWrapper(localName = "takeOverActionResponse", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.TakeOverActionResponse")
 	@Override
 	public boolean takeOverAction(String psCode, Actor actor)
 			throws UnknownActorName_Exception, UnknownCode_Exception {
@@ -26,6 +48,10 @@ public class WorkflowController implements WorkflowControllerInterface {
 		return false;
 	}
 
+	@WebMethod
+    @WebResult(name = "result", targetNamespace = "")
+    @RequestWrapper(localName = "completeAction", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.CompleteAction")
+    @ResponseWrapper(localName = "completeActionResponse", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.CompleteActionResponse")
 	@Override
 	public boolean completeAction(String actionStatusName, String nextActionName)
 			throws UnknownActionName_Exception, UnknownNextActionName_Exception {
--- src/it/polito/dp2/WF/sol4/server/WorkflowInformation.java
+++ src/it/polito/dp2/WF/sol4/server/WorkflowInformation.java
@@ -1,6 +1,7 @@
 package it.polito.dp2.WF.sol4.server;
 
 import it.polito.dp2.WF.sol4.gen.ActionStatusType;
+import it.polito.dp2.WF.sol4.gen.ObjectFactory;
 import it.polito.dp2.WF.sol4.gen.Process;
 import it.polito.dp2.WF.sol4.gen.UnknownCode_Exception;
 import it.polito.dp2.WF.sol4.gen.UnknownNames_Exception;
@@ -9,11 +10,26 @@ import it.polito.dp2.WF.sol4.gen.WorkflowInfoInterface;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlSeeAlso;
 import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.ws.Holder;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
+@WebService(name = "WorkflowInfoInterface", 
+			targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/",
+			serviceName = "WorkflowService",
+			portName = "WorkflowInfoPort",
+			endpointInterface = "it.polito.dp2.WF.sol4.gen.WorkflowInfoInterface")
+@XmlSeeAlso({ObjectFactory.class})
 public class WorkflowInformation implements WorkflowInfoInterface {
 
+	@WebMethod
+    @RequestWrapper(localName = "getWorkflowNames", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.GetWorkflowNames")
+    @ResponseWrapper(localName = "getWorkflowNamesResponse", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.GetWorkflowNamesResponse")
 	@Override
 	public void getWorkflowNames(Holder<XMLGregorianCalendar> lastModTime, Holder<List<String>> name) {
 		
@@ -21,6 +37,9 @@ public class WorkflowInformation implements WorkflowInfoInterface {
 
 	}
 
+	@WebMethod
+    @RequestWrapper(localName = "getWorkflows", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.GetWorkflows")
+    @ResponseWrapper(localName = "getWorkflowsResponse", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.GetWorkflowsResponse")
 	@Override
 	public void getWorkflows(List<String> wfName, Holder<XMLGregorianCalendar> lastModTime, Holder<List<Workflow>> workflow) 
 			throws UnknownNames_Exception {
@@ -29,6 +48,9 @@ public class WorkflowInformation implements WorkflowInfoInterface {
 
 	}
 
+	@WebMethod
+    @RequestWrapper(localName = "getProcesses", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.GetProcesses")
+    @ResponseWrapper(localName = "getProcessesResponse", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.GetProcessesResponse")
 	@Override
 	public void getProcesses(List<String> wfName, Holder<XMLGregorianCalendar> lastModTime,	Holder<List<Process>> process) 
 			throws UnknownNames_Exception {
@@ -37,6 +59,10 @@ public class WorkflowInformation implements WorkflowInfoInterface {
 
 	}
 
+	@WebMethod
+    @WebResult(name = "returnValues", targetNamespace = "")
+    @RequestWrapper(localName = "getActions", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.GetActions")
+    @ResponseWrapper(localName = "getActionsResponse", targetNamespace = "http://lucamannella.altervista.org/WorkflowManager/", className = "it.polito.dp2.WF.sol4.gen.GetActionsResponse")
 	@Override
 	public List<ActionStatusType> getActions(String psCode) throws UnknownCode_Exception {
 		// ---	This method is not required by the specifications.	--- //
