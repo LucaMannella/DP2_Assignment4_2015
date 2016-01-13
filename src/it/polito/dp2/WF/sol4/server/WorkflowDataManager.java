74df82baf474cf46222f2e78b18b08cf4608d6bd
s175637 <s175637@studenti.polito.it>
2016-01-13 13:57:20 +0100

    A DataManager class was implemented

---
+++ src/it/polito/dp2/WF/sol4/server/WorkflowDataManager.java
@@ -0,0 +1,11 @@
+package it.polito.dp2.WF.sol4.server;
+
+import it.polito.dp2.WF.WorkflowMonitor;
+
+public class WorkflowDataManager {
+
+	public WorkflowDataManager(WorkflowMonitor wfMonitor) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
