c0c73b8ae6288dd4671d9b28258b160222df1588
Luke <luca.mannella@outlook.it>
2016-01-18 02:35:32 +0100

    JavaDoc improved.

--- src/it/polito/dp2/WF/sol4/client1/ProcessActionR.java
+++ src/it/polito/dp2/WF/sol4/client1/ProcessActionR.java
@@ -2,6 +2,7 @@ package it.polito.dp2.WF.sol4.client1;
 
 import java.util.Map;
 
+import it.polito.dp2.WF.ActionReader;
 import it.polito.dp2.WF.ProcessActionReader;
 import it.polito.dp2.WF.WorkflowReader;
 import it.polito.dp2.WF.sol4.gen.ActionType;
@@ -9,12 +10,10 @@ import it.polito.dp2.WF.sol4.gen.ActionType.ProcessAction;
 
 /**
  * This is a concrete implementation of abstract class AbstractActionReader (that implements the interface ActionReader).<BR>
- * Another implementation of that abstract class is {@link it.polito.dp2.WF.sol4.client1.SimpleActionR}<br>
- * This implementation is based on JAX-WS framework.<BR><BR>
- * If you want more detail about the interface look to<BR>
- * {@link it.polito.dp2.WF.sol4.client1.AbstractActionReader}<BR>
- * {@link it.polito.dp2.WF.ActionReader}
- * 
+ * Another implementation of that abstract class is {@link SimpleActionR}<br>
+ * This implementation is based on JAX-WS framework.
+ *
+ * @see {@link ActionReader}, {@link AbstractActionReader}, {@link SimpleActionR}
  * @author Luca
  */
 public class ProcessActionR extends AbstractActionReader implements ProcessActionReader {
