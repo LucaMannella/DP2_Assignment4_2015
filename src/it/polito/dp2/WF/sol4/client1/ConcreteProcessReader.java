955101f36ee65ff13f661dc6a38e0461c3151b77
Luke <luca.mannella@outlook.it>
2016-01-19 15:26:36 +0100

    NullPointerException fixed and toShortString() method added.

--- src/it/polito/dp2/WF/sol4/client1/ConcreteProcessReader.java
+++ src/it/polito/dp2/WF/sol4/client1/ConcreteProcessReader.java
@@ -3,6 +3,7 @@ package it.polito.dp2.WF.sol4.client1;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.LinkedList;
 import java.util.List;
 
 import it.polito.dp2.WF.ActionStatusReader;
@@ -28,6 +29,7 @@ public class ConcreteProcessReader implements ProcessReader, Comparable<ProcessR
 //TODO:	if(process == null) return;	//safety lock
 		this.startTime = process.getStarted().toGregorianCalendar();
 		
+		statusActions = new LinkedList<ActionStatusReader>();
 		for( ActionStatusType action : process.getActionStatus() ) {
 			ActionStatusReader asr = new ConcreteActionStatusReader(action, myWorkflow.getName());
 			statusActions.add(asr);
@@ -53,15 +55,30 @@ public class ConcreteProcessReader implements ProcessReader, Comparable<ProcessR
 	public String toString() {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
 		
-		StringBuffer buf = new StringBuffer("Process related to workflow: "+myWorkflow.getName()+" ");
-		buf.append("Started at: "+dateFormat.format(startTime.getTimeInMillis())+"\n");
+		StringBuffer buf = new StringBuffer("Process related to workflow <"+myWorkflow.getName()+"> ");
+		buf.append("started at <"+dateFormat.format(startTime.getTimeInMillis())+">\n");
 		
 		for(ActionStatusReader asr : statusActions) {
-			buf.append(asr.toString()+"\n");
+			buf.append("\t"+asr.toString()+"\n");
 		}
 		return buf.toString();
 	}
 	
+	/**
+	 * This method gives a short version of the toString method.
+	 * 
+	 * @return A shorted version of the toStrin method.
+	 */
+	public String toShortString() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+		
+		StringBuffer buf = new StringBuffer("Process started at ");
+		buf.append( dateFormat.format(startTime.getTimeInMillis()) );
+		buf.append(" with "+statusActions.size()+" ActionStatus.");
+		
+		return buf.toString();
+	}
+
 	/**
 	 * The comparison is based on the starting time. 
 	 * If the times are equal, it is based on the name of the relative workflows.
